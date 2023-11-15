85c27e1dce715fbcff7ca6a40559cb2ce53964d6
SimonVillalonIT <simonvillalon9@gmail.com>
2023-11-15 14:27:57 -0300

    Little changes

--- Website/app/dashboard/layout.tsx
+++ Website/app/dashboard/layout.tsx
@@ -23,7 +23,7 @@ function DashboardLayout({ children }: PropsWithChildren) {
   }, [])
   return (
     <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-      <aside className="hidden w-[200px] flex-col md:flex">
+      <aside className="hidden w-[200px] flex-col sticky md:flex">
         <DashboardNav />
       </aside>
       <main className="flex w-full flex-1 flex-col overflow-hidden">
