12ae791259f12acd82a233d816eb259c90382fe8
SimonVillalonIT <simonvillalon9@gmail.com>
2023-12-11 15:17:22 -0300

    feat Settings page inputs

--- Website/app/dashboard/settings/page.tsx
+++ Website/app/dashboard/settings/page.tsx
@@ -1,90 +1,21 @@
 "use client"
 
-import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "components/ui/form"
-import { Input } from "components/ui/input"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "components/ui/select"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "components/ui/card"
 import { Separator } from "components/ui/separator"
-import { toast } from "components/ui/use-toast"
-import { useFieldArray, useForm } from "react-hook-form"
-import * as z from "zod"
+import { Switch } from "components/ui/switch"
 
-import { cn } from "@/lib/utils"
-
-const profileFormSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: "Username must be at least 2 characters.",
-    })
-    .max(30, {
-      message: "Username must not be longer than 30 characters.",
-    }),
-  email: z
-    .string({
-      required_error: "Please select an email to display.",
-    })
-    .email(),
-  bio: z.string().max(160).min(4),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
-    )
-    .optional(),
-})
-
-type ProfileFormValues = z.infer<typeof profileFormSchema>
-
-// This can come from your database or API.
-const defaultValues: Partial<ProfileFormValues> = {
-  bio: "I own a computer.",
-  urls: [
-    { value: "https://shadcn.com" },
-    { value: "http://twitter.com/shadcn" },
-  ],
-}
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 function SettingsPage() {
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(profileFormSchema),
-    defaultValues,
-    mode: "onChange",
-  })
-
-  const { fields, append } = useFieldArray({
-    name: "urls",
-    control: form.control,
-  })
-
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
-  }
-
   return (
     <section>
       <div className="space-y-0.5">
@@ -92,15 +23,54 @@ function SettingsPage() {
         <p className="text-muted-foreground">
           Maneja los niveles que consideres adecuados para tu institución
         </p>
-        <p className="text-muted-foreground/80 text-sm font-light">Los niveles que estan establecidos por defecto son los que recomendamos</p>
+        <p className="text-muted-foreground/80 text-sm font-light">
+          Los niveles que estan establecidos por defecto son los que
+          recomendamos
+        </p>
       </div>
       <Separator className="my-6" />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <Button type="submit">Actualizar configuraciones</Button>
-          </form>
-        </Form>
+        <Card>
+          <CardHeader>
+            <CardTitle>Niveles aceptados</CardTitle>
+            <CardDescription>
+              Administra los niveles que consideres aceptados.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-6 grid-cols-2">
+            <div className="flex flex-col items-center justify-between space-x-2">
+              <Label htmlFor="necessary" className="flex flex-col ">
+                <span>Strictly Necessary</span>
+              </Label>
+              <Input
+                name="max-acepted"
+                max={200}
+                min={0}
+                type="number"
+                defaultValue={30}
+              />
+            </div>
+
+            <div className="flex flex-col items-center justify-between space-y-2 space-x-2 ">
+              <Label htmlFor="functional" className="flex flex-col ">
+                <span>Functional Cookies</span>
+              </Label>
+              <Input
+                name="max-medium"
+                max={200}
+                min={0}
+                type="number"
+                value={30}
+                defaultValue={30}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button variant="outline" className="w-full">
+              Save preferences
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     </section>
   )
